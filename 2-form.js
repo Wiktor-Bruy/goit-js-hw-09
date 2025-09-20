6864417d49f4571a24c240bb455fa033f500833e
Wiktor-Bruy <holmyliszag@ukr.net>
2025-09-20 15:05:23 +0000

    Deploying to gh-pages from @ Wiktor-Bruy/goit-js-hw-09@fafb9b506c0232a6184da7c79a0e3b623f49b63f 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const a=document.querySelector(".feedback-form");let e={email:"",message:""};a.addEventListener("input",t=>{const m=t.target;e.email=m.elements.email.value.trim(),e.message=m.elements.message.value.trim(),console.log(e)});
+//# sourceMappingURL=2-form.js.map
